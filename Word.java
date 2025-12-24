902523b6f4a2b314acd2cca454be190bba91843d
SOUMIK ROY <soumikroy7272@gmail.com>
2025-12-25 01:19:19 +0530

    Solved new Question

---
+++ Word.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Word {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int upperCount = 0;
+        int lowerCount = 0;
+
+        for(int i=0; i<s.length(); i++){
+            if(Character.isUpperCase(i)){
+                upperCount++;
+            }else{
+                lowerCount++;
+            }
+        }
+        if(upperCount>lowerCount){
+            System.out.println(s.toUpperCase());
+        }else{
+            System.out.println(s.toLowerCase());
+        }
+    }
+}
