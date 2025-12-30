55f607b0422ea39b54cb997c0b450c040d5b1afd
SOUMIK ROY <soumikroy7272@gmail.com>
2025-12-31 02:29:16 +0530

    Solved another question

--- horseShoe.java
+++ horseShoe.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 import java.util.HashSet;
 public class horseShoe {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         HashSet<Integer> distinctColors = new HashSet<>();
         for (int i = 0; i < 4; i++) {
-            if (scanner.hasNextInt()) {
-                int color = scanner.nextInt();
+            if (sc.hasNextInt()) {
+                int color = sc.nextInt();
                 distinctColors.add(color);
             }
         }
