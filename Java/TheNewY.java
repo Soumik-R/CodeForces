abf187272081bdf8e7d9937c3ee49ace8dd80cb4
SOUMIK ROY <soumikroy7272@gmail.com>
2026-01-14 20:16:05 +0530

    Solved new question

---
+++ Java/TheNewY.java
@@ -0,0 +1,15 @@
+package Java;
+import java.util.*;
+public class TheNewY {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int des = sc.nextInt();
+
+        int a1 = Math.abs(des- a);
+        int b1 = Math.abs(des - b);
+        int sum = a1 + b1;
+        System.out.println(sum);
+    }
+}
