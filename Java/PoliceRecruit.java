64a91a265b19c8affd272d8b620e5163758108df
SOUMIK ROY <soumikroy7272@gmail.com>
2026-01-15 23:24:31 +0530

    Soled another question

---
+++ Java/PoliceRecruit.java
@@ -0,0 +1,23 @@
+package Java;
+import java.util.*;
+public class PoliceRecruit {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int avail = 0;
+        int unTreat = 0;
+        for(int i =0; i<n; i++){
+            int ev = sc.nextInt();
+            if(ev == -1){
+                if(avail>0){
+                    avail--;
+                }else{
+                    unTreat++;
+                }
+            }else{
+                avail+=ev;
+            }
+        }
+        System.out.println(unTreat);
+    }
+}
